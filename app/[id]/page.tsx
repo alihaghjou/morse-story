4b57a8d12c0b180e742f6a24e72605e1385fd589
alihaghjou <alihaghjou30@gmail.com>
2024-01-06 15:05:16 +0330

    created the page for showing story in morse and translate function

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,17 +1,36 @@
-import { MorseDoc } from '@/utils/supabase/Morse';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
+import { MorseDoc } from "@/utils/supabase/Morse";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
-export default async function Page({params} : {params: {id: string}}) {
-  const cookieStore = cookies()
+function translateFunc(originalText: string, MorseText?: string) {
+    //only works for text to morse for now
+  const lowered = originalText.toLowerCase();
+  const keys = Object.keys(MorseDoc);
+  const values = Object.values(MorseDoc);
+  let TempMorse = "";
+  for (let i = 0; i < lowered.length; i++) {
+    if (lowered[i] === " ") {
+      TempMorse += " " + "/";
+    } else {
+      const index = keys.indexOf(lowered[i]);
+      TempMorse += " " + values[index];
+    }
+  }
+  return TempMorse;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: notes } = await supabase.from("Story").select().eq("id", params.id);
-
+  const {
+    data: story,
+  }: { data: { id: number; created_at: string; story: string[] } | null } =
+    await supabase.from("Story").select().eq("id", params.id).maybeSingle();
   return (
     <div>
-        <p>
-            T
-        </p>
+      {story?.story.map((para) => (
+        <p>{translateFunc(para)}</p>
+      ))}
     </div>
-  )
+  );
 }
