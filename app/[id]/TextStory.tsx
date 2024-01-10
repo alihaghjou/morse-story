1327384167aed05ca2b74038e58241d6b8d39550
alihaghjou <alihaghjou30@gmail.com>
2024-01-10 12:21:53 +0330

    translation paragraph complete

--- app/[id]/TextStory.tsx
+++ app/[id]/TextStory.tsx
@@ -3,12 +3,33 @@
 import { translateFunc } from "@/utils/supabase/Translate";
 import { useState } from "react";
 
-export default function TextStory({para}: {para: string}) {
-    const [paragraph, setPara] = useState(translateFunc("text-morse", para))
+export default function TextStory({ para }: { para: string }) {
+  const [paragraph, setPara] = useState({
+    text: translateFunc("text-morse", para),
+    textType: "morse",
+  });
+
+  function handleTranslatePara() {
+    if (paragraph.textType === "morse") {
+      setPara({ text: para, textType: "text" });
+    } else {
+      setPara({
+        text: translateFunc("text-morse", para),
+        textType: "morse",
+      });
+    }
+  }
+
   return (
     <>
-    <p>{paragraph}</p>
-    <button onClick={() => setPara(para)}>Translate</button>
+      <p>{paragraph.text}</p>
+      <button onClick={handleTranslatePara}>
+        {paragraph.textType === "morse" ? (
+          <span>Translate</span>
+        ) : (
+          <span>Back</span>
+        )}
+      </button>
     </>
   );
 }
